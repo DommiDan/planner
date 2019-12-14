b9c572cd0b5a74872d7f5fac1dc099812be5db25
Leticia Guerra <g.leticia@rocketmail.com>
2019-12-14 15:33:12 -0500

    stylizing it better

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 
 $(document).ready(function(){
-    console.log(document)
+
     $(".saveBtn").on("click",function() {
         console.log(this)
         var value = $(this).siblings(".description").val();
         var time = $(this).siblings(".hour").text();
 
-        localStorage.setItem(time, value)
+        localStorage.setItem(time, value);
     });
     $("#currentDay").text(moment());
 });
+
+var myStorage = window.localStorage;
