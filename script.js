f80fffe2be90b7b30a5683f7261cc437595d396f
Leticia Guerra <g.leticia@rocketmail.com>
2019-12-14 14:41:21 -0500

    Update cal

---
+++ script.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+    console.log(document)
+    $(".saveBtn").on("click",function() {
+        console.log(this)
+        var value = $(this).siblings(".description").val();
+        var time = $(this).siblings(".hour").text();
+
+        localStorage.setItem(time, value)
+    });
+    $("#currentDay").text(moment());
+});
